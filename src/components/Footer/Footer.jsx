90a181a0e0c487540dd26e0096b66711957b3c6b
WinPhyoEi <winphyoei113@gmail.com>
2020-11-22 02:35:54 +0630

    add current year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import './Footer.css';
-const Footer = () => {  
+const Footer = () => {   
     return (
-        <div className="Footer" > 
-          <Grid item xs={12} > 
-            Copyright 2020, All right reserved.
-          </Grid>           
+        <div className="Footer" >             
+          <Grid item xs={12} >             
+            Copyright {(new Date().getFullYear())}, All right reserved.
+          </Grid>              
         </div>
     );
 }
