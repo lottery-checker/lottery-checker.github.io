d02e13a75160de1a6e463cc421f64e935dc2f2de
WinPhyoEi <winphyoei113@gmail.com>
2020-11-22 02:44:23 +0630

    add year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import './Footer.css';
-const Footer = () => {   
+const { DateTime } = require("luxon");
+const Footer = () => {      
     return (
         <div className="Footer" >             
           <Grid item xs={12} >             
-            Copyright {(new Date().getFullYear())}, All right reserved.
+            Copyright {DateTime.local().c.year}, All right reserved.
           </Grid>              
         </div>
     );
