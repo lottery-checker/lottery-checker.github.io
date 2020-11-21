5eae307e58c8aa3a1e1ad153f233b2b39af10137
WinPhyoEi <winphyoei113@gmail.com>
2020-11-22 00:42:47 +0630

    about update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -15,23 +15,23 @@ const useStyles = makeStyles((theme) => ({
 const About = () => {
     const classes=useStyles();
     return (
-        <Container className='About'>
+        <Container>
           <Grid container spacing={3}>
           <Grid item xs={12} sm={12}>
-            <h3 className='about-text'>About Page </h3>
+            <h3 className='About__header'>About Us </h3>
             <p > Lottery Checker App is non profit app for checking Myanmar lottery results.This app is live at here(https://lottery-checker.github.io)</p>
             <h3 className='about-text'>Contributors</h3>
           </Grid> 
           <Grid item xs={12}>          
-            <div className="about-logo">                 
-                <Avatar alt="Remy Sharp" src="https://avatars2.githubusercontent.com/u/49815662?s=400&u=18ad3740a825466714b856fc2a715ec8b4d63807&v=4" className={classes.large} />       
-                <Avatar alt="Remy Sharp" src="https://avatars1.githubusercontent.com/u/47824384?s=460&u=91c5d3625b88c9b716ec0edf422e13d731010425&v=4" className={classes.large}/>        
-                <Avatar alt="Remy Sharp" src="https://avatars3.githubusercontent.com/u/41888392?s=460&u=5a25a4bddceb857735cd9678ceb9b3e8ea7a3568&v=4"   className={classes.large}/>           
+            <div className="About__logo">                 
+                <Avatar alt="" src="https://avatars2.githubusercontent.com/u/49815662?s=400&u=18ad3740a825466714b856fc2a715ec8b4d63807&v=4" className={classes.large} />       
+                <Avatar alt="SArkar Htet" src="https://avatars1.githubusercontent.com/u/47824384?s=460&u=91c5d3625b88c9b716ec0edf422e13d731010425&v=4" className={classes.large}/>        
+                <Avatar alt="WinPhyoEi" src="https://avatars3.githubusercontent.com/u/41888392?s=460&u=5a25a4bddceb857735cd9678ceb9b3e8ea7a3568&v=4"   className={classes.large}/>           
             </div>
             </Grid>
            
             <Grid item xs={12}>
-                <h3 className='about-text'>Open Source</h3>
+                <h3 className='About__header'>Open Source</h3>
                 <p>https://lottery-checker.github.io/</p>
             </Grid>
           </Grid>
