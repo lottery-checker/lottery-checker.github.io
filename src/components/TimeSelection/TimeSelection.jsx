8747fc241fafb71a0daae9669b46ec9c37b505a5
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-11-22 23:07:56 +0630

    test: added unit test for TimeSelection

--- src/components/TimeSelection/TimeSelection.jsx
+++ src/components/TimeSelection/TimeSelection.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './TimeSelection.css';
 import { useStateValue } from '../../provider'
-const TimeSelection = () => {
+export const TimeSelection = () => {
     const [ { times }] = useStateValue();
     return (
         <div className='TimeSelection'>
             <select className='TimeSelection__select'>
                 {
-                    times.map(({ time, label }, index) => (
-                        <option key={index} value={time}>{label}</option>
+                    times.map(({ sheetid, label }, index) => (
+                        <option key={index} value={sheetid}>{label}</option>
                     ))
                 }
             </select>
         </div>
     )
 }
-export default TimeSelection
+export default TimeSelection;
